a48c3f68896db6bd0c935de43a7312d244f43cd1
Sareen Shah <SShah50@northwell.edu>
2019-08-03 12:41:27 -0400

    fix hash fxn

--- db_structure.py
+++ db_structure.py
@@ -557,7 +557,7 @@ class Column():
 			self.tables.add(table)
 
 	def __hash__(self):
-		return hash((self.name, self.tables))
+		return hash((self.name, tuple(self.tables)))
 
 	def __eq__(self, other):
 		return (self.name == other.name) and (self.tables == other.tables)
