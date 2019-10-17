17626379ab28aaeb3ca9a3d5a08a8da8f06a29a8
Sareen Shah <SShah50@northwell.edu>
2019-10-17 11:14:26 -0400

    disable werkzeug logging to stdout

--- run_application.py
+++ run_application.py
@@ -1,13 +1,13 @@
 from gevent import monkey
 monkey.patch_all()
-from gevent.pywsgi import WSGIServer
-from web import flask_app
-import logging
+from gevent.pywsgi import WSGIServer  # noqa: E402
+from web import flask_app  # noqa: E402
+import logging  # noqa: E402
 
 if __name__ == '__main__':
     logging.info('Starting externally accessible server')
     
+    # https://stackoverflow.com/questions/49038678/cant-disable-flask-werkzeug-logging
     LISTEN = ('0.0.0.0', 8050)
-    http_server = WSGIServer(LISTEN, flask_app)
+    http_server = WSGIServer(LISTEN, flask_app, log=None)
     http_server.serve_forever()
-
